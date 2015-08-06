c34125398eea8f848965aace350f032b3222d1e6
Jeff Austin & Logan Wu <student@epicodus.com>
2015-08-06 12:35:17 -0700

    add search and post functions.

--- app/app.php
+++ app/app.php
@@ -2,67 +2,56 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Car.php";
 
+    session_start();
+
+    if (empty($_SESSION['list_of_cars'])) {
+        $_SESSION['list_of_cars'] = array();
+    }
+
     $app = new Silex\Application();
 
-    $app->get("/new_car", function() {
-        return "
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-            <title>Find a Car</title>
-        </head>
-        <body>
-            <div class='container'>
-                <h1>Find a Car!</h1>
-                <form action='/view_car'>
-                    <div class='form-group'>
-                        <label for='price'>Enter Maximum Price:</label>
-                        <input id='price' name='price' class='form-control' type='number'>
-                        <label for='miles'>Enter Maximum Miles:</label>
-                        <input id='miles' name='miles' class='form-control' type='number'>
-                    </div>
-                    <button type='submit' class='btn-success'>Submit</button>
-                </form>
-            </div>
-        </body>
-        </html>
-        ";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function() use ($app) {
+
+        return $app['twig']->render('home.html.twig');
     });
 
-    $app->get("/view_car", function() {
-        $porsche = new Car("2014 Porsche 911", 114991, 7864, "images/porsche.jpg");
-        $ford = new Car("2011 Ford F450", 55995, 14241, "images/ford.jpg");
-        $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "images/lexus.jpg");
-        $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "images/benz.jpg");
+    $app->get('/post', function() use ($app) {
 
-        $cars = array($porsche, $ford, $lexus, $mercedes);
+    return $app['twig']->render('cars.html.twig', array('cars' => Car::getAll()));
+    });
 
+    $app->post("/cars", function() use ($app) {
+        $car = new Car($_POST['model'], $_POST['price'], $_POST['miles'], $_POST['image_path']);
+        $car->save();
+
+        return $app['twig']->render('create_car.html.twig', array('newcar' => $car));
+    });
+
+
+
+
+
+
+    $app->get("/view_car", function() use ($app) {
         $cars_matching_search = array();
+        $cars = Car::getAll();
         foreach ($cars as $car) {
-            if ($car->getPrice() < $_GET["price"] && $car->getMiles() < $_GET["miles"]) {
+            if ($car->getPrice() < $_GET["price"] && $car->getMiles() < $_GET["miles"])
+             {
                 array_push($cars_matching_search, $car);
             }
         }
+        return $app['twig']->render('car_posting.html.twig', array('cars' => $cars_matching_search));
+    });
+
+    $app->get("/search", function() use ($app) {
 
 
-        $output = "";
-        $output .= "<h1>Car Dealership</h1>";
-            if (empty($cars_matching_search)) {
-                $output .= "<h2>No matching result!</h2>";
-              } else {
-                foreach ($cars_matching_search as $car) {
-                    $output .= "<li>". $car->getMakeModel() . "</li>
-                    <ul>
-                        <li><img src=" . $car->getImage() ." width='300'></li>
-                        <li> $" . $car->getPrice() . "</li>
-                        <li> Miles:" . $car->getMiles() . "</li>
-                    </ul>";
-                }
-            }
-
-            return $output;
-        });
+        return $app['twig']->render('search_cars.html.twig');
+    });
 
     return $app;
 ?>
--- src/Car.php
+++ src/Car.php
@@ -1,53 +1,67 @@
 <?php
 class Car
 {
-    private $make_model;
-    private $price;
-    private $miles;
-    private $image_path;
+        private $make_model;
+        private $price;
+        private $miles;
+        private $image_path;
 
-    function __construct($car_model, $car_price, $car_miles, $car_pic)
-    {
-      $this->make_model = $car_model;
-      $this->price = $car_price;
-      $this->miles = $car_miles;
-      $this->image_path = $car_pic;
+        function __construct($car_model, $car_price, $car_miles, $car_pic)
+        {
+          $this->make_model = $car_model;
+          $this->price = $car_price;
+          $this->miles = $car_miles;
+          $this->image_path = $car_pic;
+        }
+
+        function getMakeModel()
+        {
+          return $this->make_model;
+        }
+
+        function setMakeModel($new_model)
+        {
+          $this->make_model = (string) $new_model;
+        }
+
+        function getPrice()
+        {
+          return $this->price;
+        }
+
+        function setPrice($new_price)
+        {
+          $this->price = (integer) $new_price;
+        }
+
+        function getMiles()
+        {
+          return $this->miles;
+        }
+
+        function setMiles()
+        {
+          $this->miles = (integer) $new_miles;
+        }
+
+        function getImage()
+        {
+          return $this->image_path;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_cars'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_cars'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_cars'] = array();
+        }
     }
-
-    function getMakeModel()
-    {
-      return $this->make_model;
-    }
-
-    function setMakeModel($new_model)
-    {
-      $this->make_model = (string) $new_model;
-    }
-
-    function getPrice()
-    {
-      return $this->price;
-    }
-
-    function setPrice($new_price)
-    {
-      $this->price = (integer) $new_price;
-    }
-
-    function getMiles()
-    {
-      return $this->miles;
-    }
-
-    function setMiles()
-    {
-      $this->miles = (integer) $new_miles;
-    }
-
-    function getImage()
-    {
-      return $this->image_path;
-    }
-}
-
 ?>
