986d446d6ddce71f9bd9b1d04ed10bfee2b76e0e
Jeff Austin <jeffaustin81@gmail.com>
2015-08-06 21:55:50 -0700

    Fixed search, added styling, cleaned up indentation

--- app/app.php
+++ app/app.php
@@ -21,26 +21,31 @@
     $app->get('/post', function() use ($app) {
 
     return $app['twig']->render('cars.html.twig', array('cars' => Car::getAll()));
+
     });
 
     $app->post("/cars", function() use ($app) {
         $car = new Car($_POST['model'], $_POST['price'], $_POST['miles'], $_POST['image_path']);
+
         $car->save();
 
         return $app['twig']->render('create_car.html.twig', array('newcar' => $car));
     });
 
-
-
-
-
+    $app->post("/delete_car", function() use ($app) {
+        Car::deleteAll();
+        return $app['twig']->render('delete_cars.html.twig', array('cars' => Car::getAll()));
+    });
 
     $app->get("/view_car", function() use ($app) {
         $cars_matching_search = array();
         $cars = Car::getAll();
+        $user_price = $_GET["price"];
+        $user_miles = $_GET["miles"];
+
         foreach ($cars as $car) {
-            if ($car->getPrice() < $_GET["price"] && $car->getMiles() < $_GET["miles"])
-             {
+            if ($car->getPrice() < $user_price && $car->getMiles() < $user_miles)
+            {
                 array_push($cars_matching_search, $car);
             }
         }
@@ -48,8 +53,6 @@
     });
 
     $app->get("/search", function() use ($app) {
-
-
         return $app['twig']->render('search_cars.html.twig');
     });
 
